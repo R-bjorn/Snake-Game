ba5ba7cdf9c2a186c988b9c03bab5f0667469289
Ravi Trivedi <81584201+R-bjorn@users.noreply.github.com>
2021-04-04 17:24:22 -0400

    editing the startUP function
    Adding some methods to use when the game starts.
    startUp funciton now initialize gameOver to false, 
    initializing position of x and y for the snake, 
    initializing position for fruit and score to 0;

--- main.cpp
+++ main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>		// For Input/Output
+#include <ctime> 		// For time()
 using namespace std;
 
+// All the variables we need for the game
 // For game status
 bool gameOver;
+// Width and Height of the game Box
+const int width = 40;
+const int height = 20;
+// Position of the snake, fruit and calculating score
+int x,y, fruitX, fruitY, score;
+// enum for direction
+enum eDirection {STOP=0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
 
 // Starting function 
 void StartUP()
 {
-	
+	// At beginning, game status is false.
+	gameOver = false;
+	// The snake is not moving
+	dir = STOP 
+	// snake starting position will be in the middle
+	x = width / 2;
+	y = height / 2;
+	// Position of the fruit is random
+	srand(time(0));
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	// Score will be 0 
+	score = 0;	
 }
 
 // Function to draw Borders, Snack and Fruit
